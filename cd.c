052a503168cbd164f952d879453e47882267c53b
Jennifer Ye <jensome6065@gmail.com>
2023-11-21 14:50:34 -0500

    added err() to cd file

---
+++ cd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int err() {
+    printf("errno: %d \n", errno);
+    printf("%s \n", strerror(errno));
+    exit(1);
+}
+
+void chDir(char* input) {
+
+}
