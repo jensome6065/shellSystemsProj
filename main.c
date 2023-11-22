ea7c3e0b8e236221ce5a385b31f68cd05ce63e2a
ellenyu-06 <eyu40@stuy.com>
2023-11-22 00:56:05 -0500

    separating by ; prog

--- main.c
+++ main.c
@@ -10,16 +10,28 @@
 int main() {
     char input[1024];
     char* argArray[64];
+    char* check;
+    char** carr = NULL;
 
     printf("enter a command: ");
     fgets(input, sizeof(input), stdin);
-
+    char* ip = &input;
     if (input[strlen(input) - 1] == '\n') {
         input[strlen(input) - 1] = '\0';
     }
-    
-    parse(input, argArray);
-    execvp(argArray[0], argArray);
+    int i = 0;
+    while ((check = strsep(&ip, ";"))!=NULL){
+        carr = (char**)realloc(carr, (i+1)*sizeof(char*));
+        carr[i] = check;
+        i++;
+    }
+    for (int j = 0; j<i; j++){
+        parse(carr[j], argArray);
+        execvp(carr[j], argArray);
+    }
+    //account for pipes and semicolons
+    // parse(input, argArray);
+    // execvp(argArray[0], argArray);
     
     return 0;
 }
