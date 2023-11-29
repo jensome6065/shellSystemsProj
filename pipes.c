686cadde1ffb729c5ae0ef6b2ed707d3dd2f274c
ellenyu-06 <eyu40@stuy.com>
2023-11-29 12:40:08 -0500

    PIPE!

--- pipes.c
+++ pipes.c
@@ -14,6 +14,7 @@ void pipee(char* comm){//pipe, redir, readme, textfile, comments, sigs
     char* part2;
     char* copy = strdup(comm);
     char* argarr[30];
+    char* argarr2[30];
     part1 = strsep(&comm, "|");
     if (part1[strlen(part1)-1]==' ') part1[strlen(part1)-1] = '\0';
     parse(part1, argarr);
@@ -34,18 +35,21 @@ void pipee(char* comm){//pipe, redir, readme, textfile, comments, sigs
     } 
     pid_t w = wait(&status);
     if(w==-1) err();
-    parse(part2, argarr);
-    int stdincopy = dup(STDIN_FILENO);
-    dup2(temp, STDIN_FILENO);
+    parse(part2, argarr2);
+    int i = 0;
+    while(argarr2[i]!=NULL){
+        i++;
+    }
+    argarr2[i] = "tempfile";
+    argarr2[i+1] = NULL;
     dup2(stdoutcopy, STDOUT_FILENO);    
     pid_t child1 = fork();
     if(child1<0) {
         err();
     } else if (child1 == 0) {
-        if (execvp(part2, argarr)==-1) err();
+        if (execvp(part2, argarr2)==-1) err();
     } 
     pid_t w1 = wait(&status);
     if(w1==-1) err();
-    dup2(stdincopy, STDIN_FILENO);
     close(temp);
  }
