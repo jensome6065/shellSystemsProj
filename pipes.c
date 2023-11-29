2078e24691e3fdf65a1fbaee2eacefea32b62fd9
ellenyu-06 <eyu40@stuy.com>
2023-11-29 10:16:23 -0500

    piping wait issue

--- pipes.c
+++ pipes.c
@@ -13,7 +13,7 @@ void pipee(char* comm){//pipe, redir, readme, textfile, comments, sigs
     char* part1;
     char* part2;
     char* copy = strdup(comm);
-    char** argarr[30];
+    char* argarr[30];
     part1 = strsep(&comm, "|");
     if (part1[strlen(part1)-1]==' ') part1[strlen(part1)-1] = '\0';
     parse(part1, argarr);
@@ -24,29 +24,35 @@ void pipee(char* comm){//pipe, redir, readme, textfile, comments, sigs
         err();
     }
     int stdoutcopy = dup(STDOUT_FILENO);
-    dup2(temp, 1);
+    dup2(temp, STDOUT_FILENO);
     pid_t child = fork();
-    pid_t w = 0;
+    //pid_t w = 0;
     int status;
     if(child<0) {
         err();
-    } else if (child == 0) {
-        execvp(part1, argarr);
-        w = wait(&status);
+    } else if (child == 0) {//program that sleeps and outputs text
+        if (execvp(part1, argarr)==-1) err();
+        // else w=getpid();
+        // w = wait(&status);
+        // if(w==-1) err();
     } 
-    waitpid(w, &status, 0);
+    //waitpid(child, &status, 0);
     parse(part2, argarr);
-    dup2(stdoutcopy, 1);    
+    dup2(stdoutcopy, STDOUT_FILENO);    
     int stdincopy = dup(STDIN_FILENO);
-    dup2(temp, 0);
+    dup2(temp, STDIN_FILENO);
     child = fork();
     if(child<0) {
         err();
     } else if (child == 0) {
-        execvp(part2, argarr);
-        w = wait(&status);
+        if (execvp(part2, argarr)==-1) err();
+        // else w=getpid();
+        // w = wait(&status);
+        // if(w==-1) err();
     } 
-    waitpid(w, &status, 0); 
-    dup2(stdincopy, 0);
+    // waitpid(child, &status, 0); 
+    // pid_t w = wait(&status);
+    // if(w==-1) err();
+    dup2(stdincopy, STDIN_FILENO);
     close(temp);
  }
