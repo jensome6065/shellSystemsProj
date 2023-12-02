d1d0105651f788d743ba83b675b4be8b0afb2e1c
ellenyu-06 <eyu40@stuy.com>
2023-12-02 06:15:01 -0500

    accounting for redirect in pipe

--- pipes.c
+++ pipes.c
@@ -7,6 +7,7 @@
 #include <sys/wait.h>
 #include "shell.h"
 #include "pipes.h"
+#include "redirect.h"
 #include "err.h"
 
 void pipee(char* comm){
@@ -15,9 +16,9 @@ void pipee(char* comm){
     char* copy = strdup(comm);
     char* argarr[30];
     char* argarr2[30];
+    int status;
     part1 = strsep(&comm, "|");
     if (part1[strlen(part1)-1]==' ') part1[strlen(part1)-1] = '\0';
-    parse(part1, argarr);
     comm ++;
     part2 = comm;
     int temp = open("tempfile", O_RDWR | O_CREAT | O_TRUNC, 0777);
@@ -25,31 +26,68 @@ void pipee(char* comm){
         err();
     }
     int stdoutcopy = dup(STDOUT_FILENO);
-    dup2(temp, STDOUT_FILENO);
-    pid_t child = fork();
-    int status;
-    if(child<0) {
-        err();
-    } else if (child == 0) {
-        if (execvp(part1, argarr)==-1) err();
-    } 
-    pid_t w = wait(&status);
-    if(w==-1) err();
-    parse(part2, argarr2);
-    int i = 0;
-    while(argarr2[i]!=NULL){
-        i++;
+    // dup2(temp, STDOUT_FILENO);
+    if (strchr(part1, '<') != NULL || strchr(part1, '>') != NULL){
+        parse(part1, argarr);
+        redirect(part1, argarr);
     }
-    argarr2[i] = "tempfile";
-    argarr2[i+1] = NULL;
-    dup2(stdoutcopy, STDOUT_FILENO);    
-    pid_t child1 = fork();
-    if(child1<0) {
-        err();
-    } else if (child1 == 0) {
-        if (execvp(part2, argarr2)==-1) err();
-    } 
-    pid_t w1 = wait(&status);
-    if(w1==-1) err();
+    else {
+        dup2(temp, STDOUT_FILENO);
+        parse(part1, argarr);
+        pid_t child = fork();
+        if(child<0) {
+            err();
+        } else if (child == 0) {
+            if (execvp(part1, argarr)==-1) err();
+        } 
+        pid_t w = wait(&status);
+        if(w==-1) err();
+    }
+    // pid_t child = fork();
+    // int status;
+    // if(child<0) {
+    //     err();
+    // } else if (child == 0) {
+    //     if (execvp(part1, argarr)==-1) err();
+    // } 
+    // pid_t w = wait(&status);
+    // if(w==-1) err();
+    if (strchr(part2, '<') != NULL || strchr(part2, '>') != NULL){
+        parse(part2, argarr2);
+        redirect(part2, argarr2);
+    }
+    else {
+        parse(part2, argarr2);
+        int i = 0;
+        while(argarr2[i]!=NULL){
+            i++;
+        }
+        argarr2[i] = "tempfile";
+        argarr2[i+1] = NULL;
+        dup2(stdoutcopy, STDOUT_FILENO);    
+        pid_t child1 = fork();
+        if(child1<0) {
+            err();
+        } else if (child1 == 0) {
+            if (execvp(part2, argarr2)==-1) err();
+        } 
+        pid_t w1 = wait(&status);
+        if(w1==-1) err();
+    }
+    // int i = 0;
+    // while(argarr2[i]!=NULL){
+    //     i++;
+    // }
+    // argarr2[i] = "tempfile";
+    // argarr2[i+1] = NULL;
+    // dup2(stdoutcopy, STDOUT_FILENO);    
+    // pid_t child1 = fork();
+    // if(child1<0) {
+    //     err();
+    // } else if (child1 == 0) {
+    //     if (execvp(part2, argarr2)==-1) err();
+    // } 
+    // pid_t w1 = wait(&status);
+    // if(w1==-1) err();
     close(temp);
  }
