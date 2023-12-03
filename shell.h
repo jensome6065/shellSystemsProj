d41f747c025603fb891f16840a856cdc636dd42e
Jennifer Ye <jensome6065@gmail.com>
2023-12-03 10:56:04 -0500

    added comments for parse and changeDir

--- shell.h
+++ shell.h
@@ -2,7 +2,16 @@
 #define SHELL_H
 
 void parse(char* line, char** argArray);
+// parses the input line into an array of arguments
+// char* line       the input line to be parsed 
+// char** argArray  an array of strings that stores the parsed arguments; last element set to NULL
+// returns void 
+
 void exitCom();
+
 void changeDir(char* input);
+// changes the current working directory using chDir
+// char* input  the target directory path; if NULL, change to parent directory ("..")
+// returns void
 
 #endif
