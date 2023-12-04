ea39224903e355cb2b3a52e1b2957bb3e84be9b1
Jennifer Ye <jensome6065@gmail.com>
2023-12-03 20:08:41 -0500

    added redirect function comment

--- redirect.h
+++ redirect.h
@@ -4,7 +4,17 @@
 void redirect(char* command, char** args);
 // executes commands with input/output redirection
 // char* command    the command to be executed
-// char** args      an array of arguments for the command
+// char** args      an array of strings with the command and its arguments
 // returns void
+/* 
+redirect forks a new process that executes the specified command with '<' or '>'
+'<' opens the specified reading file and sets it as stdin for the command
+opened input redirection file is duplicated and stdin is redirected to the duplicated file
+'>' opens or creates the specified writing file and sets it as stdout
+opened output redirection file is duplicated and stdout is redirected to the duplicated file
+corresponding command and argument elements are set to NULL
+err() is called if file does not exist, cannot be found, cannot be opened or created, or if execvp fails
+original stdin and stdout are restored after execution
+*/
 
 #endif
