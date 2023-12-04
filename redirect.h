a2ce863ebd53de5f5b75bc285833a3cc9eab1df1
Jennifer Ye <jensome6065@gmail.com>
2023-12-03 20:08:26 -0500

    added redirect function comment

--- redirect.h
+++ redirect.h
@@ -2,5 +2,9 @@
 #define REDIRECT_H
 
 void redirect(char* command, char** args);
+// executes commands with input/output redirection
+// char* command    the command to be executed
+// char** args      an array of arguments for the command
+// returns void
 
 #endif
